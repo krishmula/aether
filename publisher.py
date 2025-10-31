c8dc2b2d6744e925db2916a238a01d748c7d0f39
Kevin Jin <59934204+kevwjin@users.noreply.github.com>
2025-10-31 11:00:05 -0700

    remove unnecessary function

--- publisher.py
+++ publisher.py
@@ -1,10 +1,8 @@
 """Publisher implementation."""
-import random
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from broker import Broker
-from uint8 import UInt8
 from message import Message
 
 
@@ -15,10 +13,6 @@ class Publisher:
 
     def __init__(self, broker: "Broker") -> None:
         self._broker = broker
-
-    def _create_msg(self) -> Message:
-        i = random.randint(0, 255)
-        return Message(UInt8(i))
 
     def publish(self, msg: Message) -> None:
         self._broker.publish(msg)
